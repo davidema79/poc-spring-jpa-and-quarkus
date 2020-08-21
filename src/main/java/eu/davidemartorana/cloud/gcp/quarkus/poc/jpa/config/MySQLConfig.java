606941a8b213aa3f97651b7609c92cbb13a91e6a
Davide Martorana <davide.martorana@gmail.com>
2020-08-21 18:15:32 +0100

    - Updated version - Removed useless files

--- src/main/java/eu/davidemartorana/cloud/gcp/quarkus/poc/jpa/config/MySQLConfig.java
+++
@@ -1,4 +0,0 @@
-package eu.davidemartorana.cloud.gcp.quarkus.poc.jpa.config;
-
-public class MySQLConfig {
-}
--- src/main/java/eu/davidemartorana/cloud/gcp/quarkus/poc/jpa/config/PostgreSQLConfig.java
+++
@@ -1,5 +0,0 @@
-package eu.davidemartorana.cloud.gcp.quarkus.poc.jpa.config;
-
-public class PostgreSQLConfig {
-
-}
